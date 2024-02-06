703ad293a3ca4ccd9303b8881660da0ff71af1d0
yoavuzan <yoav547@gmail.com>
2024-02-06 12:47:31 +0200

    with docker compose

---
+++ src/main/java/com/handson/backend/FirstProjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.handson.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjBackendApplication.class, args);
+	}
+
+}
