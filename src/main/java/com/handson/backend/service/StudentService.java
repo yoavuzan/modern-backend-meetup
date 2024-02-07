58ab5e59b0671a233e54411e7235767cbdbe7d5a
yoavuzan <yoav547@gmail.com>
2024-02-07 11:39:11 +0200

    with FPS

--- src/main/java/com/handson/backend/service/StudentService.java
+++ src/main/java/com/handson/backend/service/StudentService.java
@@ -5,6 +5,7 @@ import com.handson.backend.repo.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,6 +29,10 @@ public class StudentService {
 
     public void delete(Student student) {
         repository.delete(student);
+    }
+
+    public List<Student> getStudentWithSatHigherThan(Integer sat) {
+        return repository.findAllBySatScoreGreaterThan(sat);
     }
 
 }
